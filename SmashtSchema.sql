9ab4be507fb59027c1e3c8feb22fe030a8290441
ballardcz <ballardcz@gmail.com>
2018-06-03 18:59:53 -0700

    doing some work

--- SmashtSchema.sql
+++ SmashtSchema.sql
@@ -4,37 +4,33 @@ CREATE database Smasht;
 USE Smasht;
 
 CREATE TABLE USERS (
-_firstname varchar (255) NOT NULL,
-_lastname varchar (255) NOT NULL,
-_idUser int (10) NOT NULL AUTO_INCREMENT,
-_email varchar (255),
-PRIMARY KEY (_idUser)
+    _idUser int (10) NOT NULL AUTO_INCREMENT,
+    _lastname varchar (255) NOT NULL,
+    _firstname varchar (255) NOT NULL,
+    _email varchar (255),
+    _rating int (10),
+    PRIMARY KEY (_idUser)
 );
 
 CREATE TABLE Trivia (
-_title varchar (255) NOT NULL,
-_category varchar (255) NOT NULL,
-_rating INT (10) NOT NULL,
-_level varchar (255)NOT NULL,
-_idTrivia int (10) NOT NULL AUTO_INCREMENT,
-_tags varchar (25) 
-PRIMARY KEY (_idTrivia) 
+    _idTrivia int (10) NOT NULL AUTO_INCREMENT,
+    _title varchar (255) NOT NULL,
+    _category varchar (255) NOT NULL,
+    _rating INT (10) NOT NULL,
+    _level varchar (255)NOT NULL,
+    _tags varchar (25), 
+    PRIMARY KEY (_idTrivia) 
 );
 
 CREATE TABLE Questions (
-_idquestion int (10) NOT NULL AUTO_INCREMENT,
-_quest TINYTEXT NOT NULL,
-
+    _idquestion int (10) NOT NULL AUTO_INCREMENT,
+    _quest TINYTEXT NOT NULL,
+    _tags varchar (25),
+    PRIMARY KEY (_idquestion)
 );
 
 CREATE TABLE Answers (
-    _ans varchar (255)
-)
-
-
--_title
--_category 
--_rating
--_level
--_primary key(id)
--_tags
+    _idAns int (10) AUTO_INCREMENT NOT NULL,
+    _ans varchar (255) NOT NULL,
+    PRIMARY KEY (_idAns)
+);
